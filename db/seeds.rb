99d3a940ff2b418ac9d7b0c0b44c334b82e29b4e
JackMarx <seriousfools@gmail.com>
2019-05-06 15:49:15 -0500

    restful routes

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+contact = Contact.new(first_name: "Jeff", last_name: "Bridges", email: "thedude@rug.org", phone_number: "(310) 555-1234")
+contact.save
+
+contact = Contact.new(first_name: "Sharon", last_name: "Smith", email: "sharon@rug.org", phone_number: "(310) 555-1734")
+contact.save
+
+contact = Contact.new(first_name: "Carol", last_name: "Merry", email: "bells@christmas.org", phone_number: "(310) 555-1098")
+contact.save
+
+contact = Contact.new(first_name: "Timmy", last_name: "Cratchet", email: "ccarol@dickens.net", phone_number: "(310) 555-0001")
+contact.save
